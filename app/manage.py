86c3a8696346d9b4bccb903ea63a36f41c2f61d6
artisakov <art.isackov@gmail.com>
2020-09-01 22:29:36 +0300

    better

--- app/manage.py
+++ app/manage.py
@@ -32,7 +32,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///diacompanion.db'
 app.config['TESTING'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'teos.sicrets@gmail.com'
